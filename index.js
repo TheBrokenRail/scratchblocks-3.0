daa05cba109a2263b7102b823a88e7ff748ece47
TheBrokenRail <connor24nolan@live.com>
2017-05-20 11:28:10 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+var ScratchBlocks = require("scratch-blocks");
+
+window.onload = function() {
+
+    var options = {
+        readOnly: true,
+        css: true,
+        media: './media/',
+        rtl: false,
+        scrollbars: false,
+        sounds: true,
+        oneBasedIndex: true
+    };
+
+    var div = document.createElement("DIV");
+    div.id = "ScratchBlocksWorkspace";
+    document.body.appendChild(div);
+    div.style.visibility = "hidden";
+    div.style.position = "absolute";
+
+    /* Inject your workspace */
+    var workspace = ScratchBlocks.inject("ScratchBlocksWorkspace", options);
+
+    var blocks = document.getElementsByClassName("scratchblocks");
+    for (i = 0; i < blocks.length; i++) {
+        if (blocks[i].tagName.toUpperCase() === "XML") {
+            ScratchBlocks.Xml.domToWorkspace(blocks[i], workspace);
+            var draggables = document.getElementsByClassName("blocklyBlockCanvas");
+            if (draggables.length > 0) {
+                var svg = document.createElementNS('http://www.w3.org/2000/svg','svg');
+                svg.setAttribute("class", "scratchblocks");
+                svg.setAttribute("height", draggables[0].getBoundingClientRect().height);
+                svg.setAttribute("width", draggables[0].getBoundingClientRect().width);
+                svg.setAttribute("viewBox", draggables[0].getBBox().x + " " + draggables[0].getBBox().y + " " + draggables[0].getBBox().width + " " + draggables[0].getBBox().height);
+                svg.appendChild(draggables[0].cloneNode(true));
+                blocks[i].parentElement.replaceChild(svg, blocks[i]);
+            }
+            workspace.clear();
+        }
+    }
+
+}
